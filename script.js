2c183e40850326e6ef15ba176e5689a92b05e7d4
mrespicio <meganrespicio@hotmail.com>
2023-02-16 11:12:08 -0800

    buttons will change style when clicked on

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const gridContainer = document.getElementById('grid-container')
 
+
+
 const smBtn = document.getElementById('small');
 const mdBtn = document.getElementById('medium');
 const lgBtn = document.getElementById('large');
 
 smBtn.addEventListener('click', () => {
+	lgBtn.classList.remove('btn-selected');
+	mdBtn.classList.remove('btn-selected');
 	smBtn.setAttribute('class', 'btn-selected');
 	resetGrid();
 	createGrid(8)
 });
 mdBtn.addEventListener('click', () => {
+	smBtn.classList.remove('btn-selected');
+	mdBtn.classList.remove('btn-selected');
 	mdBtn.setAttribute('class', 'btn-selected');
 	resetGrid();
 	createGrid(16);
 });
 lgBtn.addEventListener('click', () => {
+	smBtn.classList.remove('btn-selected');
+	mdBtn.classList.remove('btn-selected');
 	lgBtn.setAttribute('class', 'btn-selected');
 	resetGrid();
 	createGrid(32);
 });
+
 
 let dimension;
 function createGrid(dimension){
@@ -44,11 +53,6 @@ function resetGrid(){
 	while(gridContainer.lastElementChild){
 		gridContainer.removeChild(gridContainer.lastElementChild)
 	}
-}
-
-function removeBtnStyle(btn){
-	console.log('removed')
-	btn.setAttribute("style", "");
 }
 
 // default grid
