389e584a602a28c507c741a7184b14f25bf9bcfb
mrespicio <meganrespicio@hotmail.com>
2023-02-15 18:49:22 -0800

    add buttons to change grid dimensions; in progress

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const gridContainer = document.getElementById('grid-container')
 
+const smBtn = document.getElementById('small');
+const mdBtn = document.getElementById('medium');
+const lgBtn = document.getElementById('large');
+
+let dimension;
+smBtn.addEventListener('click', () => {
+	resetGrid();
+	createGrid(8)
+});
+mdBtn.addEventListener('click', () => createGrid(16))
 
 // buttons
 // each button has a dimension
@@ -13,11 +23,21 @@ function createGrid(dimension){
 	for(i = 0; i < (Math.pow(dimension, 2)); i++){
 		let item = document.createElement('div');
 		item.setAttribute('class', 'grid-item')
-		item.addEventListener('mouseover', () => item.classList.add('fill'))
+		item.addEventListener('mouseover', () => { item.classList.add('fill') });
+		/*item.addEventListener('mouseover', () => { 
+			if(item.classList.contains('fill')){
+			item.classList.add('fill-2')}
+		});
+		item.addEventListener('mouseover', () => item.classList.add('fill')) */
 		gridContainer.appendChild(item);
 	}
 }
 
-// input
-createGrid(10);
+// loop grid and remove nodes
+// while child node exists, remove it
+function resetGrid(){
 
+}
+
+// input
+createGrid(16);
